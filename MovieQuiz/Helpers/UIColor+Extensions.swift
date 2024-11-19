76a5426cf0139962e5f786057e9e17df01ab1c74
Mat <me@me.me>
2024-11-19 11:09:13 +0300

    Tried to change to lowercase

--- MovieQuiz/Helpers/UIColor+Extensions.swift
+++ MovieQuiz/Helpers/UIColor+Extensions.swift
@@ -2,10 +2,10 @@ import UIKit
 import Foundation
 
 extension UIColor {
-    static var yBlack: UIColor { UIColor(named: "YP back") ?? UIColor.black}
-    static var yRed: UIColor { UIColor(named: "YP red") ?? UIColor.red}
-    static var yGreen: UIColor { UIColor(named: "YP green") ?? UIColor.green}
-    static var yBackground: UIColor { UIColor(named: "YP background") ?? UIColor.lightGray}
-    static var yGray: UIColor { UIColor(named: "YP gray") ?? UIColor.gray}
-    static var yWhite: UIColor { UIColor(named: "YP white") ?? UIColor.white}
+    static var yBlack: UIColor { UIColor(named: "YP Black") ?? UIColor.black}
+    static var yRed: UIColor { UIColor(named: "YP Red") ?? UIColor.red}
+    static var yGreen: UIColor { UIColor(named: "YP Green") ?? UIColor.green}
+    static var yBackground: UIColor { UIColor(named: "YP Background") ?? UIColor.lightGray}
+    static var yGray: UIColor { UIColor(named: "YP Gray") ?? UIColor.gray}
+    static var yWhite: UIColor { UIColor(named: "YP White") ?? UIColor.white}
 }
